86ad690b234394ff74887eb165060313b7c6461a
SurajCB <121911751+SurajCB@users.noreply.github.com>
2023-06-11 13:58:39 +0530

    Add files via upload

---
+++ Chat gpt powered smart ai chat bot.py
@@ -0,0 +1,21 @@
+#this code is developed by Suraj CB
+#Github Profile URL: https://github.com/SurajCB
+#This code is contributed to be an Open Source Program
+import openai
+
+openai.api_key = "your api openai api key"
+
+messages = []
+system_msg = input("What type of chatbot would you like to create?\n")
+messages.append({"role": "system", "content": system_msg})
+
+print("Your new assistant is ready!")
+while input != "quit()":
+    message = input()
+    messages.append({"role": "user", "content": message})
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages)
+    reply = response["choices"][0]["message"]["content"]
+    messages.append({"role": "assistant", "content": reply})
+    print("\n" + reply + "\n")
